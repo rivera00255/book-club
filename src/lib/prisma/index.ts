99cd11c8b249e37e850ebbb5208283eb3a80af3a
river <rivera00255@gmail.com>
2023-10-22 18:40:15 +0900

    :wrench: edit prisma

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -2,7 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const global = globalThis as unknown as { prisma: PrismaClient };
 
-const prisma = global.prisma || new PrismaClient();
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    datasourceUrl: process.env.POSTGRES_PRISMA_URL,
+  });
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
