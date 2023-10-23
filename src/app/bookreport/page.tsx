183175b669315bd57ceea94412125f3558dd7bf6
river <rivera00255@gmail.com>
2023-10-23 12:07:50 +0900

    :wrench: edit

--- src/app/bookreport/page.tsx
+++ src/app/bookreport/page.tsx
@@ -1,19 +1,23 @@
 import ReportList from "@/components/ReportList";
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
+import { sql } from "@vercel/postgres";
 
 const BookReport = async () => {
-  const reports = await prisma.report.findMany({
-    orderBy: [
-      {
-        createdAt: "desc",
-      },
-    ],
-  });
+  const client = await sql.connect();
+  const { rows } = await client.sql`SELECT * FROM database`;
+  console.log(rows);
+  // const reports = await prisma.report.findMany({
+  //   orderBy: [
+  //     {
+  //       createdAt: "desc",
+  //     },
+  //   ],
+  // });
 
   return (
     <main>
       <h2>독서 기록</h2>
-      {reports && <ReportList reports={reports} />}
+      {/* {reports && <ReportList reports={reports} />} */}
     </main>
   );
 };
--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -2,15 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const global = globalThis as unknown as { prisma: PrismaClient };
 
-let prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
