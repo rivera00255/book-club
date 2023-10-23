fefaa658c7153c146e5860b353faa928f486be60
river <rivera00255@gmail.com>
2023-10-23 12:46:53 +0900

    Revert ":wrench: edit scripts"
    This reverts commit 1a804091b22045a5b27cbcca47c3496fd2c47ce0.

--- src/app/bookreport/page.tsx
+++ src/app/bookreport/page.tsx
@@ -1,23 +1,19 @@
 import ReportList from "@/components/ReportList";
-// import prisma from "@/lib/prisma";
-import { sql } from "@vercel/postgres";
+import prisma from "@/lib/prisma";
 
 const BookReport = async () => {
-  const client = await sql.connect();
-  const { rows } = await client.sql`SELECT * FROM database`;
-  console.log(rows);
-  // const reports = await prisma.report.findMany({
-  //   orderBy: [
-  //     {
-  //       createdAt: "desc",
-  //     },
-  //   ],
-  // });
+  const reports = await prisma.report.findMany({
+    orderBy: [
+      {
+        createdAt: "desc",
+      },
+    ],
+  });
 
   return (
     <main>
       <h2>독서 기록</h2>
-      {/* {reports && <ReportList reports={reports} />} */}
+      {reports && <ReportList reports={reports} />}
     </main>
   );
 };
