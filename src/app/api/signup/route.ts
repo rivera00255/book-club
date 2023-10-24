9ac7a4c450d3f9e5a94b9af93807007017439b59
river <rivera00255@gmail.com>
2023-10-24 10:37:06 +0900

    :wrench: + prisma

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
+import prisma from "@/lib/prisma";
 
 export const POST = async (request: Request) => {
   try {
@@ -10,25 +11,16 @@ export const POST = async (request: Request) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
 
-    // const user = await prisma.user.create({
-    //   data: {
-    //     email,
-    //     password: hashPassword,
-    //   },
-    // });
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashPassword,
+      },
+    });
 
-    // return new NextResponse(
-    //   JSON.stringify({
-    //     user: { email: user.email, createdAt: user.createdAt },
-    //   }),
-    //   {
-    //     status: 201,
-    //     headers: { "Content-Type": "application/json" },
-    //   }
-    // );
     return new NextResponse(
       JSON.stringify({
-        user: { email: "", createdAt: "" },
+        user: { email: user.email, createdAt: user.createdAt },
       }),
       {
         status: 201,
